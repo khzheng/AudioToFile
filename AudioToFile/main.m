5df55bc7b6cd622de86aea511d03164eeb132879
Ken Zheng <kzheng@ucdavis.edu>
2014-07-23 13:50:17 -0700

    Added utility functions

--- AudioToFile/main.m
+++ AudioToFile/main.m
@@ -17,6 +17,31 @@
 
 #pragma mark - Utility
 
+static char *StringFromOSStatusError(OSStatus error, char *str) {
+    bzero(str, sizeof(str));
+    
+    // see if it appears to be a 4-char-code, look in AudioFile.h for mapping
+    *(UInt32 *)(str + 1) = CFSwapInt32HostToBig(error);
+    if (isprint(str[1]) && isprint(str[2]) && isprint(str[3]) && isprint(str[4])) {
+        str[0] = str[5] = '\'';
+        str[6] = '\0';
+    } else {
+        // Nope, format it as integer
+        sprintf(str, "%d", (int)error);
+    }
+    return str;
+}
+
+void CheckError(OSStatus error, const char *operation) {
+    if (error == noErr) return;
+    
+    char str[20];
+    char *errorStr = StringFromOSStatusError(error, str);
+    fprintf(stderr, "AudioServicesError: %s (%s)", operation, errorStr);
+    
+    [NSException raise:@"NSInternalInconsistencyException" format:@"AudioServicesError: %s (%i: %s)\n backtrace:\n %@", operation, (int)error, errorStr, [NSThread callStackSymbols]];
+}
+
 int main(int argc, const char * argv[])
 {
 
