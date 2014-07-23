7fa70d8a61678926a84404bf5e4d928161511f1f
Ken Zheng <kzheng@ucdavis.edu>
2014-07-23 13:38:36 -0700

    First commit

---
+++ AudioToFile/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AudioToFile
+//
+//  Created by Ken Zheng on 7/23/14.
+//  Copyright (c) 2014 Toktumi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
